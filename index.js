7758f9fd2944921012fd45e1eaa36299187a916e
thunder04 <70254851+thunder04@users.noreply.github.com>
2021-02-11 14:02:13 +0200

    Update index.js
    Just learned that centra has a built-in json method so why not use it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const centra = require("centra")
 
-const resolveOutput = (_) => JSON.parse(_.body.toString() || "{}")
+const resolveOutput = (res) => res.json();
 const BASE_URL = "https://nekos.best";
 const ENDPOINTS = [
     'cuddle', 'feed',
@@ -14,14 +14,14 @@ class NekoBestClient {
     constructor() {
         ENDPOINTS.forEach((endpoint) => {
             this[endpoint === 'nekos' ? 'getNeko' : `get${endpoint[0].toUpperCase() + endpoint.slice(1).toLowerCase()}`] = async function () {
-                return await centra(`${BASE_URL}/${endpoint}`).send().then(resolveOutput).then((res) => res.url)
+                return await centra(`${BASE_URL}/${endpoint}`).send().then(resolveOutput).then((res) => res ? res.url : null)
                     .catch(() => null);
             }
         })
 
         this['getRandom'] = async function () {
             const endpoint = ENDPOINTS[Math.floor(Math.random() * ENDPOINTS.length)];
-            return await centra(`${BASE_URL}/${endpoint}`).send().then(resolveOutput).then((res) => res.url)
+            return await centra(`${BASE_URL}/${endpoint}`).send().then(resolveOutput).then((res) => res ? res.url : null)
                 .catch(() => null);
         }
     }
