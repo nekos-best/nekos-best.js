18091a81de1041e218292ebc74a1346627125484
thunder04 <70254851+thunder04@users.noreply.github.com>
2022-02-06 21:18:06 +0200

    refactor: Change error messages and remove ArgumentError

--- index.ts
+++ index.ts
@@ -3,25 +3,26 @@ import req from 'petitio';
 const forceRange = (x: number, min: number, max = Infinity) => min > max ? 0 : Math.max(Math.min(x, max), min)
 const BASE_PATH = "https://nekos.best/api/v1";
 const ENDPOINTS = [
-    'smile', 'smug', 'tickle',
+    'baka', 'bite', 'blush',
+    'bored', 'cry', 'cuddle',
+    'dance', 'facepalm', 'feed',
+    'happy', 'highfive', 'hug',
     'kiss', 'laugh', 'nekos',
-    'baka', 'cry', 'cuddle',
-    'dance', 'feed', 'hug',
-    'pat', 'poke', 'slap',
-    'wave', 'bite', 'blush',
-    'bored', 'facepalm', 'happy',
-    'highfive', 'pout', 'shrug',
-    'sleep', 'stare', 'think',
-    'thumbsup', 'wink'
+    'pat', 'poke', 'pout',
+    'shrug', 'slap', 'sleep',
+    'smile', 'smug', 'stare',
+    'think', 'thumbsup', 'tickle',
+    'wave', 'wink'
 ] as const;
 
 export async function fetchNeko<T extends nbEndpoints>(type: T, amount: number, options?: { min?: number, max?: number }): Promise<nbResponse[] | null>
 export async function fetchNeko<T extends nbEndpoints>(type: T, amount?: number, options?: { min?: number, max?: number }): Promise<nbResponse | null>
 export async function fetchNeko<T extends nbEndpoints>(type: T, amount?: number, { min = -1, max = -1 } = {}): Promise<unknown> {
-    if (typeof amount !== 'undefined' && !Number.isSafeInteger(amount)) throw new ArgumentError(amount, 'amount', 'safe integer')
-    if (!ENDPOINTS.includes(type)) throw new ArgumentError(type, 'type', ENDPOINTS.join(', '))
-    if (!Number.isSafeInteger(min)) throw new ArgumentError(min, 'min', 'safe integer')
-    if (!Number.isSafeInteger(max)) throw new ArgumentError(max, 'max', 'safe integer')
+    if (!ENDPOINTS.includes(type)) throw new TypeError(`Unknown category for 'type'. Received '${type}'. Available endpoints: ${ENDPOINTS.join(', ')}`)
+    if (typeof amount !== 'undefined' && !Number.isSafeInteger(amount)) throw new TypeError(`Expected a safe integer for 'amount'. Received ${typeof amount}`)
+    if (!Number.isSafeInteger(min)) throw new TypeError(`Expected a safe integer for 'min'. Received ${typeof min}`)
+    if (!Number.isSafeInteger(max)) throw new TypeError(`Expected a safe integer for 'max'. Received ${typeof max}`)
+
     if (min > max) [min, max] = [max, min]
 
     if (typeof amount === 'number') return req(`${BASE_PATH}/${type}?amount=${forceRange(amount, 0)}`).json()
@@ -36,11 +37,7 @@ export async function fetchNeko<T extends nbEndpoints>(type: T, amount?: number,
     max = forceRange(max, Number(limits.min), Number(limits.max))
     min = forceRange(min, Number(limits.min), Number(limits.max))
 
-    return { url: `${BASE_PATH}/${type}/${String(min + Math.random() * (max - min) | 0).padStart(limits.max.length, '0')}.${limits.format}` }
-}
-
-class ArgumentError extends Error {
-    constructor(arg: any, name: string, type: string) { super(`[${name}] Expected ${type}. Received type of ${typeof arg}`) }
+    return { url: `${BASE_PATH}/${type}/${String(min + Math.floor(Math.random() * (max - min))).padStart(limits.max.length, '0')}.${limits.format}` }
 }
 
 export type nbLimits = { [k in nbEndpoints]: { min: string, max: string, format: string } }
